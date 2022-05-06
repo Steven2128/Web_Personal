17ad5f86a7c2ce970752f76f92d367c8d4208e09
Stephen Alarcón <slj212885@gmail.com>
2022-05-05 21:54:21 -0500

    Implementando Cloudinary

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,13 @@
 # Django
 from django.db import models
+# Cloudinary
+from cloudinary.models import CloudinaryField
 
 class Project(models.Model):
     """Model Project"""
     title = models.CharField(max_length=200, verbose_name="Título del proyecto")
     description = models.TextField(max_length=200, verbose_name="Descripción del proyecto")
-    image = models.ImageField(upload_to='projects/', verbose_name="Imagen del proyecto")
+    image = models.CloudinaryField('imagen', blank=True, null=True, folder="media/projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación del proyecto")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización del proyecto")
 
--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-#import environ
 from pathlib import Path
+import cloudinary
+import cloudinary_storage
 
 #...
 #...rest will be same
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'portfolio',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +128,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+# Cloudinary stuff
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
@@ -134,7 +146,7 @@ STATICFILES_DIRS = [
 ]
 
 # Media files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
