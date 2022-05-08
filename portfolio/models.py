99536428609b1c1e8368876dce77070e53fe4d68
Stephen Alarcón <slj212885@gmail.com>
2022-05-07 22:43:51 -0500

    feat(portfolio):Añadir enlace a los proyectos

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,6 +8,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título del proyecto")
     description = models.TextField(max_length=200, verbose_name="Descripción del proyecto")
     image = models.ImageField(upload_to='projects/', verbose_name="Imagen del proyecto")
+    link = models.URLField(verbose_name="Dirección Web", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación del proyecto")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización del proyecto")
 
